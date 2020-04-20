96645fa27965baf32c46f1610280474adfe75f38
Aaron Chesley <achesley@ximasoftware.com>
2020-04-20 13:10:34 -0600

    add boolean translation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BooleanTranslationComponent } from './boolean-translation.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BooleanTranslationComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/boolean-translation.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-boolean-translation',
+  template: `
+    <div i18n>
+      {
+        value, select,
+        TRUE {Yes}
+        FALSE {No}
+      }
+    </div>
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BooleanTranslationComponent {
+  @Input() value;
+}
