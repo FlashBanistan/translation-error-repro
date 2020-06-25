bc06c3898d23302d823fee0f9570905cab74f96e
Aaron Chesley <aaron.chesley1990@gmail.com>
2020-06-24 19:59:46 -0600

    Revert "further examples of this problem... and the fix"

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BooleanTranslationComponent } from './boolean-translation.component';
-import { BooleanTranslation2Component } from './boolean-translation-2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BooleanTranslationComponent,
-    BooleanTranslation2Component
   ],
   imports: [
     BrowserModule
